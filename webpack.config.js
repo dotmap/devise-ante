858f57d2f856ed1465c79542436f1e8f4ab540e1
Graham Barber <grahammbarber@gmail.com>
2017-10-12 13:38:24 -0700

    build(webpack): Added friendly errors and dashboard support
    Notifications and analysis are important for debugging. The dev server will now notify you when you
    have any errors, and you can take a look at Webpack Dashbaord if you want more details.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
+const notifier = require('node-notifier')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackDashboardPlugin = require('webpack-dashboard/plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
@@ -12,15 +15,17 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        use: [{
-          loader: 'vue-loader',
-          options: {
-            loaders: {
-              'scss': 'vue-style-loader!css-loader!sass-loader',
-              'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+        use: [
+          {
+            loader: 'vue-loader',
+            options: {
+              loaders: {
+                scss: 'vue-style-loader!css-loader!sass-loader',
+                sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+              }
             }
           }
-        }]
+        ]
       },
       {
         test: /\.js$/,
@@ -33,30 +38,32 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            name: '[name].[ext]?[hash]',
-            limit: 10000
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[ext]?[hash]',
+              limit: 10000
+            }
           }
-        }]
+        ]
       }
     ]
   },
   resolve: {
     extensions: ['.js', '.vue'],
-    modules: [
-      'node_modules'
-    ],
+    modules: ['node_modules'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      vue$: 'vue/dist/vue.esm.js'
     }
   },
   devServer: {
+    port: 3000,
     noInfo: true,
     historyApiFallback: {
       index: '/dist/'
     },
+    quiet: true
   },
   devtool: '#eval-source-map',
   plugins: [
@@ -64,6 +71,34 @@ module.exports = {
       template: 'src/index.html'
     })
   ]
+}
+
+if (process.env.NODE_ENV === 'development') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new WebpackDashboardPlugin(),
+    new FriendlyErrorsPlugin({
+      compilationSuccessInfo: {
+        messages: [
+          'Hooray! The build was successful.',
+          'The local development url is http://localhost:3000'
+        ],
+        notes: [
+          'This build is not optimized for production.',
+          'To create a production build, run `npm run build`'
+        ]
+      },
+      onErrors: (severity, errors) => {
+        if (severity === 'error') {
+          const error = errors[0]
+          const message = error.message.match(/(^.*)/)
+          notifier.notify({
+            title: `Error in ${error.file}`,
+            message: message[0]
+          })
+        }
+      }
+    })
+  ])
 }
 
 if (process.env.NODE_ENV === 'production') {
