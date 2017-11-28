05ec2696c2a27bcb20d61dcd7b5fb13958e1ea0b
Rob Mac <robmac@maccelerated.me>
2017-11-27 19:27:23 -0800

    fix(element-nav): Preserve next element id
    Save next element number to localstorage instead of guessing from list length.
    
    re #34

--- src/games/Game.vue
+++ src/games/Game.vue
@@ -2,8 +2,11 @@
 import ElementNav from './elements/ElementNav.vue'
 import EditElement from './elements/EditElement.vue'
 
-const save = s => localStorage.setItem('elements', JSON.stringify(s))
-const load = () => JSON.parse(localStorage.getItem('elements') || '[]')
+const save = s => localStorage.setItem('daba', JSON.stringify(s))
+const load = () => JSON.parse(localStorage.getItem('daba') || 'false') || {
+  elements: [],
+  nextElement: 1
+}
 
 export default {
   name: 'Game',
@@ -22,40 +25,35 @@ export default {
     }
   },
   data () {
-    return {
-      localList: load()
-    }
+    return load()
   },
   computed: {
     element () {
-      return this.localList.find(e => e.id === this.id)
-    },
-    nextId () {
-      return `${this.localList.length + 1}`
+      return this.elements.find(e => e.id === this.id)
     }
   },
   methods: {
-    saveLocal () {
-      save(this.localList)
-    },
     create () {
-      const {gameId, nextId} = this
-      this.localList.push({id: nextId, title: nextId, markdown: `# ${nextId}\n\n`})
-      this.saveLocal()
-      this.$message({type: 'success', message: `Created Element ${nextId}`})
-      this.$router.push({name: 'game', params: {gameId, id: nextId}})
+      const {gameId, elements, nextElement} = this
+      const id = `${nextElement + 1}`
+      elements.push({id, title: id, markdown: `# ${id}\n\n`})
+      this.nextElement++
+      save({elements, nextElement: nextElement + 1})
+      this.$message({type: 'success', message: `Created Element ${id}`})
+      this.$router.push({name: 'game', params: {gameId, id}})
     },
     del ({id}) {
-      const {gameId} = this
-      const i = this.localList.findIndex(e => e.id === id)
-      this.localList.splice(i, 1)
-      this.saveLocal()
+      const {gameId, elements, nextElement} = this
+      const i = elements.findIndex(e => e.id === id)
+      elements.splice(i, 1)
+      save({elements, nextElement})
       this.$message({ type: 'success', message: 'Deleted' })
       this.$router.push({ name: 'game', params: {gameId} })
     },
     onEdit (markdown) {
+      const {elements, nextElement} = this
       this.element.markdown = markdown
-      this.saveLocal()
+      save({elements, nextElement})
     }
   }
 }
@@ -64,7 +62,7 @@ export default {
 <template>
   <el-container>
     <el-aside width="240px">
-      <element-nav :gameId="gameId" :elements="localList" @create="create" @delete="del"/>
+      <element-nav :gameId="gameId" :elements="elements" @create="create" @delete="del"/>
     </el-aside>
     <el-main>
       <edit-element v-if="element" :markdown="element.markdown" @edit="onEdit"/>
