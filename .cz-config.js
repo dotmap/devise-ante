195eb41b7450efc6388a8683e4d31c37e2c82c44
Graham Barber <grahammbarber@gmail.com>
2017-12-09 22:15:57 -0800

    pipeline(commitizen): update cz-customizable prompts
    Removes some of the... Aggression from all-caps sections of the new commit process

--- .cz-config.js
+++ .cz-config.js
@@ -2,25 +2,24 @@ module.exports = {
   types: [
     {value: 'feat',     name: 'feat:     A new feature'},
     {value: 'fix',      name: 'fix:      A bug fix'},
-    {value: 'docs',     name: 'docs:     Documentation only changes'},
-    {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'},
-    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
-    {value: 'perf',     name: 'perf:     A code change that improves performance'},
+    {value: 'docs',     name: 'docs:     Documentation-only changes'},
+    {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'},
+    {value: 'refactor', name: 'refactor: Changes that neither fix a bug nor add a feature'},
+    {value: 'perf',     name: 'perf:     Changes that improves performance'},
     {value: 'test',     name: 'test:     Adding missing tests'},
-    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'},
+    {value: 'pipeline', name: 'pipeline: Changes to the build process or auxiliary tools and libraries'},
     {value: 'revert',   name: 'revert:   Revert to a commit'},
     {value: 'WIP',      name: 'WIP:      Work in progress'}
   ],
   messages: {
     type: 'Select the type of change that you\'re committing:',
-    scope: '\nDenote the SCOPE of this change (optional):',
-    // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
-    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
-    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: 'List any BREAKING CHANGES (optional):\n',
-    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
-    confirmCommit: 'Are you sure you want to proceed with the commit above?'
+    scope: '\nDenote the scope of this change:',
+    customScope: 'Denote the scope of this change:',
+    subject: 'Write a short, imperative-tense description of the change:\n',
+    body: 'Provide a longer description of the change (optional). Use "|" to break a new line:\n',
+    breaking: 'List any breaking changes (optional):\n',
+    footer: 'List any issues closed by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Does this look good?'
   },
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix']
