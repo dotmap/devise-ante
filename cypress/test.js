db5aa96ddd6ececd91ebb48e289522165142947b
Graham Barber <grahammbarber@gmail.com>
2017-12-15 16:59:40 -0800

    pipeline(circle): add failure output for integration tests
    For testing in CI, previously the server would never error, even if tests failed. Now it will.

--- cypress/test.js
+++ cypress/test.js
@@ -8,4 +8,9 @@ cypress.run({
   browser: 'electron',
   record: process.env.RECORD_KEY || false,
   key: process.env.RECORD_KEY || null
-}).then(res => server.stop())
+}).then(res => {
+  server.stop()
+  if (res.failures > 0) {
+    throw new Error(`${res.failures} out of ${res.tests} tests failed in ${res.duration}. Take a look at the log above for details.`)
+  }
+}).catch(err => { throw new Error(err) })
